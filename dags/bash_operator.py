9917b0ee7bf93ca17fc6c51c1a6ee822d325c5ac
minyeamer <minyeamer@gmail.com>
2025-05-30 00:25:02 +0900

    add example bash operator

---
+++ dags/bash_operator.py
@@ -0,0 +1,24 @@
+from airflow import DAG
+from airflow.providers.standard.operators.bash import BashOperator
+import datetime as dt
+import pendulum
+
+with DAG(
+    dag_id="bash_operator",
+    schedule="0 0 * * *",
+    start_date=pendulum.datetime(2025, 1, 1, tz="Asia/Seoul"),
+    catchup=False,
+    dagrun_timeout=dt.timedelta(minutes=60),
+    tags=["example"],
+) as dag:
+    bash_task1 = BashOperator(
+        task_id="bash_task1",
+        bash_command="echo whoami",
+    )
+
+    bash_task2 = BashOperator(
+        task_id="bash_task2",
+        bash_command="echo $HOSTNAME",
+    )
+
+    bash_task1 >> bash_task2
